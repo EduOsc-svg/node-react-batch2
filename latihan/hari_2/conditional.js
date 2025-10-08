530edbb2f7c4261241682775bf3b77e89dd48c4e
Oscar Farassendra Vulvian Farid <t19-0042731048@smkn7-smr.sch.id>
2025-10-08 09:28:54 +0800

    chore: update deps / build

---
+++ latihan/hari_2/conditional.js
@@ -0,0 +1,72 @@
+console.log("Latihan Pengkondisian\n");
+console.log("===Pengkondisian IF===\n");
+
+if (true) {
+    console.log("Running true");
+}
+
+if (false) {
+    console.log("Running false");
+}
+
+var mood = 'happy';
+if (mood === 'happy') {
+    console.log("Happy banget eyy");
+} 
+
+console.log("\n===Pengkondisian IF-ELSE===\n");
+
+
+if (mood === 'happy') {
+    console.log("Happy banget eyy");
+} else {
+    console.log("Sedih banget eyy");
+}
+
+console.log("\n===Pengkondisian ELSE IF===\n");
+
+if (mood === 'happy') {
+    console.log("Happy banget eyy");
+} else if (mood === 'sad') {
+    console.log("Sedih banget eyy");
+} else {
+    console.log("Saya baik-baik saja");
+}
+
+console.log("\n===Pengkondisian Nested IF ===\n");
+var minimarketStatus = 'open';
+var telur = 'sold out  ';
+var buah = 'sold out';
+
+if (minimarketStatus == 'open') {
+    console.log("Saya masuk minimarket");
+    if (telur == 'sold out' && buah == 'sold out') {
+        console.log(" kedua item habis");
+    } if (telur == 'sold out' && buah == 'available') {
+        console.log("Habis telur, buah Tersedia");
+    }else if (buah == 'sold out' && telur == 'available') {
+        console.log("Habis buah, telur Tersedia");
+    }
+}else {
+        console.log("minmarket tutup mending balik");
+    }
+
+console.log("\n===Pengkondisian Switch Case===\n");
+var buttonPushed = '2';
+switch (buttonPushed) {
+    case '1':
+        console.log("Matikan TV");
+        break;
+    case '2':
+        console.log("Turunkan Volume TV");
+        break;
+    case '3':
+        console.log("Tingkatkan Volume TV");
+        break;
+    case '4':
+        console.log("Matikan TV");
+        break;
+    
+    default:
+        console.log("tidak terjadi apa-apa");
+}
