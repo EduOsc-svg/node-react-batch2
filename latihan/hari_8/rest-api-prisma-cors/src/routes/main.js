b42665fbb89be72ddee483f92e7c56709640ea48
Oscar Farassendra Vulvian Farid <t19-0042731048@smkn7-smr.sch.id>
2025-10-16 16:04:44 +0800

    Now

--- latihan/hari_8/rest-api-prisma-cors/src/routes/main.js
+++ latihan/hari_8/rest-api-prisma-cors/src/routes/main.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const { createMovie, readMovies, readMoviesByID, updateMovie, deleteMovie } = require('../controllers/movieController')
+const { basicAuth } = require('../middleware/basicAuth')
 const router = express.Router()
 
 
-router.post('/movies', createMovie)
-router.get('/movies', readMovies)
-router.get('/movies/:id', readMoviesByID)
-router.put('/movies/:id', updateMovie)
-router.delete('/movies/:id', deleteMovie)
+router.post('/movies',basicAuth ,createMovie)
+router.get('/movies', basicAuth, readMovies)
+router.get('/movies/:id', basicAuth, readMoviesByID)
+router.put('/movies/:id',basicAuth, updateMovie)
+router.delete('/movies/:id',basicAuth, deleteMovie)
+
 module.exports = router
